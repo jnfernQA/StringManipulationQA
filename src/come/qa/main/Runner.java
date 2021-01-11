06a5ad1c511896ef10ade434360700ed76281d6e
jnfernQA <JFernandes@qa.com>
2021-01-11 09:50:04 +0000

    Added Method 1 of String Manipulation Exercises

---
+++ src/come/qa/main/Runner.java
@@ -0,0 +1,13 @@
+package come.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		StringMethod SMethod = new StringMethod();
+		
+		SMethod.countWords("Sir I have come to invade your land.");
+		
+		//SMethod.printWordsVertically("hello Sir I have come to invade your land");
+	}
+
+}
